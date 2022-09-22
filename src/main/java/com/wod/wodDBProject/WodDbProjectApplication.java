28b190182e7e3adc35b410dc3828d3bd08cd2894
Nur Luski Levi <BritishPirate@users.noreply.github.com>
2022-09-22 10:45:10 +0100

    First Commit

---
+++ src/main/java/com/wod/wodDBProject/WodDbProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wod.wodDBProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WodDbProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WodDbProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wod/wodDBProject/WodDbProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wod.wodDBProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WodDbProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
